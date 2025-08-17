940dcb5beec9ad46e3a0a79321dfae2e3d9b64aa
Shubh Varshney <123425267+shubhvarshney@users.noreply.github.com>
2025-08-17 00:31:57 +0000

    Fixed error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-import { IonicModule } from '@ionic/angular';
+//import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [IonApp, IonRouterOutlet, IonicModule],
+  imports: [IonApp, IonRouterOutlet, /* IonicModule */ ],
 })
 
 export class AppComponent {
